cf21169a71e86bc9dfa1358b8dd84b5dc1c8ac3d
rolling <galixig3n@gmail.com>
2018-11-08 21:43:17 +0200

    conditional plugin entries

--- generators/app/templates/webpack/utils/index.js
+++ generators/app/templates/webpack/utils/index.js
@@ -5,17 +5,19 @@ const typePostfix = type => (type === MODERN_CONFIG ? "" : `.${type}`)
 
 const tplStrType = (str = "") => type => str.replace("[type]", typePostfix(type))
 
+const compactMap = _.flow(
+    _.map,
+    _.compact,
+)
+
 const conditionalEntries = match => (...entries) =>
-    _.flow(
-        entries.filter(entry => {
-            if (!_.isArray(entry)) return true
+    compactMap(entries, entry => {
+        if (!_.isArray(entry)) return entry
 
-            const [condition] = entry
+        const [condition, value] = entry
 
-            return condition === match
-        }),
-        _.flatten,
-    )
+        return condition === match && value
+    })
 
 module.exports = {
     tplStrType,
